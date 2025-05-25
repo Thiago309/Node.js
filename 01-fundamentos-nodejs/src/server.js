fa69356d8dbe6fafcc28bfc15e85ec0ae0a6ccff
Thiago Vinicius <116599158+Thiago309@users.noreply.github.com>
2025-05-25 17:21:54 -0300

    Criando atalho dev
    node --watch src/server.js

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,4 +1,18 @@
-const a = 5;
-const b = 5;
+import http from 'node:http'
 
-console.log(a + b)
+/* 
+    const server = http.createServer((request, response)
+
+    ou posso usar dessa outra maneira
+
+    const server = http.createServer((req, ou res)
+
+    request (req) = Recebe todas a informações da requisição que esta chegando no servidor.
+*/
+
+const server = http.createServer((req, res) => {
+    return res.end('Hello World')
+})
+
+// porta localhost:3333
+server.listen(3333)
